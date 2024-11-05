40288adbf48626614f13d1d29bbdcb284c495178
EldrichMaid <kupo@mail.ru>
2024-11-05 15:16:51 +0300

    Добавлен пример обработки исключения через try/catch/finally

--- ExeptionApp/Program.cs
+++ ExeptionApp/Program.cs
@@ -11,12 +11,33 @@
     }
     internal class Program
     {
+        static int Division(int a, int b)
+            {
+                return a / b;
+            }
         static void Main()
         {
             Exception exception = new Exception();
             exception.Data.Add("Дата исключения : ", DateTime.Now);
 
             Exception AnotherException = new("Собственное исключения") {HelpLink = "www.google.com"};
+
+            try
+            {
+                int result = Division(7, 0);
+
+                Console.WriteLine(result);
+            }
+            catch (System.DivideByZeroException)
+            {
+                Console.WriteLine("На ноль делить нельзя!");
+            }
+            finally
+            {
+                Console.WriteLine("Блок Finally сработал!");
+            }
+
+            Console.ReadKey();            
         }
     }
 }
