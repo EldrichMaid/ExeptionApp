c293e00e2263dd5bee8622a30baf27fac06a07d4
EldrichMaid <kupo@mail.ru>
2024-11-05 14:47:36 +0300

    Добавьте файлы проекта.

---
+++ ExeptionApp/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ExeptionApp
+{
+    public class MyException : Exception
+    {
+        public MyException()
+        { }
+
+        public MyException(string message)
+            : base(message)
+        { }
+    }
+    internal class Program
+    {
+        static void Main()
+        {
+            
+        }
+    }
+}
